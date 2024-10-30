96992b9e3166e1cf37eddc4646dc5d850a28b68c
Yukai Zhou <59173062+frank-thou@users.noreply.github.com>
2024-10-30 12:59:06 +0800

    Update evaluation scripts

--- experiments/eval_scripts/lastStep_eval.sh
+++ experiments/eval_scripts/lastStep_eval.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 export n=25
-export model=llama2 # choose from: gemma, gemma2, llama2, llama2_13b, llama3, llama31, mistralv02, mistralv03, qwen, vicuna_13b, vicuna
+export model=gemma # choose from: gemma, gemma2, llama2, llama2_13b, llama3, llama31, mistralv02, mistralv03, qwen, vicuna_13b, vicuna
 
-# sample file paht
+# sample file path
 file_list=(
     '../results_dsn/20240101-13:00:00 1E0/vicuna_dsn_25_offset0.json'
 )
@@ -22,7 +22,6 @@ do
         --config.eval_batchsize=25 \
         --config.eval_max_new_len=512 \
         --config.eval_target_folder_name="" \
-        --config.eval_with_repeated_sys_prompt=False \
         --config.logfile="${file_path}" \
         --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
 
@@ -37,7 +36,34 @@ do
         --config.eval_batchsize=25 \
         --config.eval_max_new_len=512 \
         --config.eval_target_folder_name="evalJBBLastStep" \
-        --config.eval_with_repeated_sys_prompt=False \
+        --config.logfile="${file_path}" \
+        --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
+
+    # evaluate upon the NIPS 2024 competition CLAS dataset (test-phase)
+    python -u ../evaluate_last_step.py \
+        --config="../configs/transfer_${model}.py" \
+        --config.train_data="../../data/nips24_contest_test.csv" \
+        --config.data_offset=0 \
+        --config.n_train_data=100 \
+        --config.n_test_data=0 \
+        --config.use_empty_system_prompt=False \
+        --config.eval_batchsize=25 \
+        --config.eval_max_new_len=512 \
+        --config.eval_target_folder_name="evalNIPSLastStep" \
+        --config.logfile="${file_path}" \
+        --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
+
+    # evaluate upon the NIPS 2024 competition CLAS dataset (development-phase)
+    python -u ../evaluate_last_step.py \
+        --config="../configs/transfer_${model}.py" \
+        --config.train_data="../../data/nips24_contest.csv" \
+        --config.data_offset=0 \
+        --config.n_train_data=100 \
+        --config.n_test_data=0 \
+        --config.use_empty_system_prompt=False \
+        --config.eval_batchsize=25 \
+        --config.eval_max_new_len=512 \
+        --config.eval_target_folder_name="evalNIPSLastStep" \
         --config.logfile="${file_path}" \
         --config.dsn_notes="For the last step, by generating 512 tokens, eval DSN by Refusal Matching...log file path is ${file_path}"
 
